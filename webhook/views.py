ffe38e0d35d6ae8893d003d56e2e56a0dff8928c
chunkai721 <chunkai721@gmail.com>
2023-10-13 01:15:32 +0800

    add line bot

--- webhook/views.py
+++ webhook/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
-import subprocess
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import subprocess
 import git
+
+
+# Initialize LINE bot
+line_bot_api = LineBotApi('d8NLd76ri5tk87nHVatdlQxwbvmFzxN31DsEjSBz4A1wCC99dnSzcRbT7sia6b4iQBPbA3kXSbUKh/Mkc4d6j8eTFOsfA+Xs1FYq1DkyXAoJaoEX3gGZirKpTo5NQwoiOHDjBVbK7gNRXkfKUMqUPAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('da4d953d7bb43d3535148d3ee59e8668')
 
 @csrf_exempt
 def update_server(request):
@@ -28,5 +34,26 @@ def update_server(request):
             return JsonResponse({'error': 'Error executing manage.py commands'}, status=500)
     else:
         return JsonResponse({'error': 'Wrong event type'}, status=400)
+    
+@csrf_exempt
+def callback(request):
+    # Get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
+    # Get request body as text
+    body = request.body.decode('utf-8')
 
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        return HttpResponse(status=400)
+
+    return HttpResponse(status=200)
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # Echo the user's message
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )    
